e463a82bac65288f098bd6e9b1cac7a0b2033166
olgasolda <olyasis@gmail.com>
2023-05-03 16:03:11 +0300

    api connection, xhr

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
-import { appInit } from "./movies.js";
+// xhr variant
 
-appInit();
+// const getData = (url) => new Promise((resolve, reject) => {
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.open('GET', url);
+
+//   xhr.send();
+
+//   xhr.onload = () => {
+//     if (xhr.status !== 200) {
+//       reject(xhr.status);
+//     }
+//     const json = JSON.parse(xhr.response);
+//     resolve(json.Search);
+//   }
+
+//   xhr.onerror = (err) => reject(err);
+// });
+
+const getData = (url) => fetch(url)
+  .then(res => res.json())
+  .then(data => data.Search);
+
+const searchString = 'Superman';
+
+getData(`http://omdbapi.com/?apikey=d8d86f03&s=${searchString}`)
+  .then((movies) => movies.forEach(movie => console.log(movie)))
+  .catch((err) => console.log(err));
