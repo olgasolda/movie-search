67d48abea01c1eb2e70bde20ae3d876cf0ec227a
olgasolda <olyasis@gmail.com>
2023-05-04 14:37:43 +0300

    style, markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,70 @@
+let moviesList = null;
+
+const createStyle = () => {
+  const headStyle = document.createElement('style');
+
+  headStyle.innerHTML = `
+  * {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  padding: 20px;
+}
+
+.movies {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+}
+
+.movie {
+  align-content: center;
+  justify-content: center;
+}
+
+.movie__image {
+  width: 100%;
+  object-fit: cover;
+}
+  `;
+
+  document.head.append(headStyle);
+}
+
+const createMarkup = () => {
+  const container = document.createElement('div');
+  const movies = document.createElement('div');
+
+
+  container.setAttribute('class', 'container');
+  movies.setAttribute('class', 'movies');
+
+
+  container.append(movies);
+  document.body.append(container);
+
+  moviesList = document.querySelector('.movies');
+}
+
+const addMoviesToList = (movie) => {
+const item = document.createElement('div');
+const img = document.createElement('img');
+
+
+item.setAttribute('class', 'movie');
+img.setAttribute('class', 'movie__image');
+img.src = movie.Poster;
+img.alt = `${movie.Title} ${movie.Year}`;
+
+item.append(img);
+moviesList.append(item);
+}
+
 const getData = (url) => fetch(url)
   .then(res => res.json())
   .then(data => data.Search);
@@ -5,9 +72,11 @@ const getData = (url) => fetch(url)
 const searchString = 'Superman';
 
 getData(`http://omdbapi.com/?apikey=d8d86f03&s=${searchString}`)
-  .then((movies) => movies.forEach(movie => console.log(movie)))
+  .then((movies) => movies.forEach(movie => addMoviesToList(movie)))
   .catch((err) => console.log(err));
 
+  createMarkup();
+  createStyle();
 
 // xhr variant
 
