13046c6a6276c449c3fe3732b15dbd8083d92cfb
olgasolda <olyasis@gmail.com>
2023-05-09 16:19:12 +0300

    done

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,9 +1,33 @@
 export let moviesList = null;
-export const createStyle = () => {
-  const headStyle = document.createElement('style');
+export let inputSearch = null;
+export let triggerMode = false;
 
-  headStyle.innerHTML = `
-  * {
+const createElement = ({ type,
+  attrs = {},
+  container = null,
+  position = 'append',
+  event = null,
+  handler = null }) => {
+  const el = document.createElement(type);
+
+  Object.keys(attrs).forEach((key) => {
+    if (key === 'innerHTML') el.innerHTML = attrs[key];
+    else el.setAttribute(key, attrs[key]);
+  });
+
+  if (container && position === 'append') container.append(el);
+  if (container && position === 'prepend') container.prepend(el);
+  if (event && handler && typeof handler === 'function') el.addEventListener(event, handler);
+
+  return el;
+};
+
+export const createStyle = () => {
+  createElement({
+    type: 'style',
+    attrs: {
+      innerHTML: `
+* {
   box-sizing: border-box;
 }
 
@@ -12,7 +36,8 @@ body {
 }
 
 .container {
-  padding: 20px;
+  width: min(100% - 40px, 1280px);
+  margin-inline: auto;
 }
 
 .movies {
@@ -30,36 +55,145 @@ body {
   width: 100%;
   object-fit: cover;
 }
-  `;
 
-  document.head.append(headStyle);
+.search {
+  margin-bottom: 30px;
 }
+
+.search__label-input {
+  display: block;
+  margin-bottom: 7px;
+}
+
+.search__input {
+  display: block;
+  max-width: 400px;
+  width: 100%;
+  padding: 10px 15px;
+  border-radius: 7px;
+  border: 1px solid lightsteelblue;
+  margin-bottom: 10px;
+}
+
+.search__label-checkbox {
+  font-size: 0.8rem;
+}
+
+.search__group--checkbox {
+  display: flex;
+  gap: 5px;
+  align-items: center;
+}
+  `
+    },
+    container: document.head
+  });
+
+};
 
 export const createMarkup = () => {
-  const container = document.createElement('div');
-  const movies = document.createElement('div');
+  const container = createElement({
+    type: 'div',
+    attrs: { class: 'container' },
+    container: document.body,
+    position: 'prepend'
+  });
 
+  createElement({
+    type: 'h1',
+    attrs: { innerHTML: 'Приложение для поиска фильмов' },
+    container
+  });
 
-  container.setAttribute('class', 'container');
-  movies.setAttribute('class', 'movies');
+  const searchBox = createElement({
+    type: 'div',
+    attrs: { class: 'search' },
+    container
+  });
 
+  const inputBox = createElement({
+    type: 'div',
+    attrs: { class: 'search__group search__group--input' },
+    container: searchBox
+  });
 
-  container.append(movies);
-  document.body.append(container);
+  const checkBox = createElement({
+    type: 'div',
+    attrs: { class: 'search__group search__group--checkbox' },
+    container: searchBox
+  });
+
+  createElement({
+    type: 'label',
+    attrs: {
+      class: 'search__label input',
+      for: 'search',
+      innerHTML: 'Текст для поиска'
+    },
+    container: inputBox
+  });
+
+  inputSearch = createElement({
+    type: 'input',
+    attrs: {
+      class: 'search__input',
+      id: 'search',
+      type: 'search',
+      placeholder: 'Начните ввод текста...'
+    },
+    container: inputBox
+  });
+
+  createElement({
+    type: 'input',
+    attrs: {
+      class: 'search__checkbox',
+      id: 'checkbox',
+      type: 'checkbox',
+    },
+    container: checkBox,
+    event: 'click',
+    handler: () => triggerMode = !triggerMode
+  });
+
+  createElement({
+    type: 'label',
+    attrs: {
+      class: 'search__label checkbox',
+      for: 'checkbox',
+      innerHTML: 'Добавлять фильмы к существующему списку'
+    },
+    container: checkBox
+  });
+
+  createElement({
+    type: 'div',
+    attrs: { class: 'movies' },
+    container
+  });
+
 
   moviesList = document.querySelector('.movies');
-}
-
-export const addMoviesToList = (movie) => {
-const item = document.createElement('div');
-const img = document.createElement('img');
-
-
-item.setAttribute('class', 'movie');
-img.setAttribute('class', 'movie__image');
-img.src = movie.Poster;
-img.alt = `${movie.Title} ${movie.Year}`;
-
-item.append(img);
-moviesList.append(item);
 };
+
+export const addMoviesToList = (m) => {
+  const item = createElement({
+    type: 'div',
+    attrs: { class: 'movie' },
+    position: 'prepend',
+    container: moviesList
+  });
+
+  createElement({
+    type: 'img',
+    attrs: {
+      class: 'movie__image',
+      src: /^(http|https):\/\//i.test(m.Poster) ? m.Poster : 'assets/img/no-image.png',
+      alt: `${m.Title} ${m.Year}`,
+      title: `${m.Title} ${m.Year}`,
+    },
+    container: item
+  });
+};
+
+export const clearMoviesMarkup = (el) => el && (el.innerHTML = '');
--- assets/js/movies.js
+++ assets/js/movies.js
@@ -2,20 +2,45 @@ import {
   createStyle,
   createMarkup,
   addMoviesToList,
-  moviesList
+  clearMoviesMarkup,
+  inputSearch,
+  moviesList,
+  triggerMode
 } from './dom.js';
+
+let searchLast = null;
+
+const debounceTime = (() => {
+  let timer = null;
+  return (cb, ms) => {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+    timer = setTimeout(cb, ms)
+  };
+})();
 
 const getData = (url) => fetch(url)
   .then(res => res.json())
   .then(data => data.Search);
 
-const searchString = 'Superman';
+const inputSearchHandler = (e) => {
+  debounceTime(() => {
+    const searchString = e.target.value.trim();
+    if (searchString && searchString.length > 3 && searchString !== searchLast) {
+      if (!triggerMode) clearMoviesMarkup(moviesList);
 
-getData(`http://omdbapi.com/?apikey=d8d86f03&s=${searchString}`)
-  .then((movies) => movies.forEach(movie => addMoviesToList(movie)))
-  .catch((err) => console.log(err));
+      getData(`http://omdbapi.com/?apikey=d8d86f03&s=${searchString}`)
+        .then((movies) => movies.forEach(movie => addMoviesToList(movie)))
+        .catch((err) => console.log(err));
+    }
+    searchLast = searchString;
+  }, 1000);
+};
 
 export const init = () => {
   createMarkup();
   createStyle();
+  inputSearch.addEventListener('input', inputSearchHandler);
 }
